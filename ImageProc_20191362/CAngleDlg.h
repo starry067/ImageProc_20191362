80940aae0561defe56335cb28ed4d01b77e026db
starry067 <55232668+starry067@users.noreply.github.com>
2023-12-11 18:04:11 +0900

    Add files via upload

---
+++ ImageProc_20191362/CAngleDlg.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CAngleDlg 대화 상자
+
+class CAngleDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CAngleDlg)
+
+public:
+	CAngleDlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CAngleDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = ID_DIALOG_ANGLEINPUT };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnEnChangeEdit1();
+	int m_iAngle;
+};
